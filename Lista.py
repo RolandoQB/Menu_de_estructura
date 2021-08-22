e3b8be66e5d97e68934456e189c12dc3a769bf21
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-08-21 21:57:03 -0500

    Update Lista.py

--- Lista.py
+++ Lista.py
@@ -57,31 +57,40 @@ class Lista_1:
             
     # busca el dato eliminar con el metodo buscar y lo elimina de la lista
     def eliminar (self,dato):
-        pass
-
+        res=self.buscar(dato)
+        if res != -1:
+            for pos,ele in enumerate(self.lista):
+                if ele== dato:
+                    elim=self.lista[pos]
+                    self.lista=self.lista[:pos] + self.lista[pos+1:]
+                    self.longitud-=1       
+                    return elim,self.lista
+        else:
+            return None
 
     def mostrar(self):
         print("{:3}{:9} {}".format("","Lista","Posicion"))
         for pos,ele in enumerate(self.lista):
             print("[{:8}]  {:5}".format(ele,pos))
 # pos=1
-lista1=Lista_1(5)
-lista1.append("Daniel")
-lista1.append(52)
-lista1.append(True)
-lista1.append("Milagro")
-lista1.mostrar()
-# print(lista1.obtener(pos))
-# posicion = int(input("ingrese posicion para obtener el elemento: "))
-# resp= lista1.obtenerEliminando(posicion)
-# if resp ==None:
-#     print("Posicion no valida, Verifique la Lista.....")
+# lista1=Lista_1(5)
+# lista1.append("Daniel")1
+# lista1.append(52)
+# lista1.append(True)
+# lista1.append("Milagro")
+# lista1.mostrar()
+# # print(lista1.obtener(pos))
+# # posicion = int(input("ingrese posicion para obtener el elemento: "))
+# # resp= lista1.obtenerEliminando(posicion)
+# # if resp ==None:
+# #     print("Posicion no valida, Verifique la Lista.....")
+# # else:
+# #     print("El elemento de la posicion: {} es: {}".format(posicion,resp))
+# res=lista1.buscar("Daniel")
+# if res != -1:
+#     print(res)
 # else:
-#     print("El elemento de la posicion: {} es: {}".format(posicion,resp))
-res=lista1.buscar("Daniel")
-if res != -1:
-    print(res)
-else:
-    print("no esta")
+#     print("no esta")
 
-lista1.insertar(0)
+# lista1.insertar(0)
+# print(lista1.eliminar(52))
